8c61b22954c19930ecfd826f759c321d44b668d0
Patrick <patrick@godknows.de>
2020-10-24 09:16:53 +0200

    Add project files.

---
+++ BlazorBlog/Shared/BlogPost.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorBlog.Shared
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string Description { get; set; }
+
+        public string Author { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        public bool IsPublished { get; set; } = true;
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
